0ba7e76a8ce7a1973472b705c851744e94fe4bb5
mariaconcettarena <mariaconcettaarena@hotmail.it>
2023-11-16 23:17:23 +0100

    Prima view design carino

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -9,7 +9,7 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 import Photos
 import PDFKit
-/*
+
 class SharedViewModel: ObservableObject {
     @Published var scannedImage: UIImage?
     @Published var savedDocuments: [URL] = []
@@ -20,232 +20,100 @@ struct ContentView: View {
     @State private var isImagePickerPresented: Bool = false
     @State private var showAlert: Bool = false
     @State private var isShowingDocumentsModal: Bool = false
-
+    
+    //for the background
+    let gradient = LinearGradient(
+        gradient: Gradient(colors: [Color.white, Color.gray, Color.black]),
+        startPoint: .topLeading,
+        endPoint: .bottomTrailing
+    )
+    
     var body: some View {
         NavigationView {
-            VStack {
-                if let scannedImage = viewModel.scannedImage {
-                    Image(uiImage: scannedImage)
-                        .resizable()
-                        .scaledToFit()
+            
+            ZStack{
+                
+                gradient
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    if let scannedImage = viewModel.scannedImage {
+                        Image(uiImage: scannedImage)
+                            .resizable()
+                            .scaledToFit()
+                            .padding()
+                        
+                        Button("Save") {
+                            applyFilter()
+                            saveImageAsPDF()
+                            // Reset scannedImage and return to "Capture Photo" screen
+                            withAnimation {
+                                viewModel.scannedImage = nil
+                            }
+                        }
+                        .buttonStyle(DoneButtonStyle())
                         .padding()
-
-                    Button("Done") {
-                        applyFilter()
-                        saveImageAsPDF()
-                        // Reset scannedImage and return to "Capture Photo" screen
-                        viewModel.scannedImage = nil
-                    }
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(RoundedRectangle(cornerRadius: 15).fill(Color.blue))
-                    .padding(.horizontal, 20)
-                    .shadow(radius: 5)
-                    .padding()
-                } else {
-                    Button(action: {
-                        isImagePickerPresented.toggle()
-                    }) {
-                        HStack {
-                            Text("Capture photo")
+                    } else {
+                        Button(action: {
+                            isImagePickerPresented.toggle()
+                        }) {
+                            CapturePhotoButton()
+                        }
+                        .sheet(isPresented: $isImagePickerPresented) {
+                            ImagePicker(selectedImage: $viewModel.scannedImage, sourceType: .camera)
+                                .onDisappear {
+                                    if viewModel.scannedImage != nil {
+                                        applyFilter()
+                                    }
+                                }
+                        }
+                        
+                        Spacer().frame(height: 40)
+                        
+                    
+                        HStack (spacing:0){
+                            Button(action: {
+                                isShowingDocumentsModal.toggle()
+                            }) {
+                                Text("View Documents")
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                            }
+                            
+                            Image(systemName: "doc.text")
                                 .foregroundColor(.white)
-                                .padding()
-                                .background(RoundedRectangle(cornerRadius: 15).fill(Color.blue))
-                                .padding(.horizontal, 20)
-                                .shadow(radius: 5)
-                                .bold()
+                                .font(.title2).offset(x: -40)
                         }
-                    }
-                    .sheet(isPresented: $isImagePickerPresented) {
-                        ImagePicker(selectedImage: $viewModel.scannedImage, sourceType: .camera)
-                            .onDisappear {
-                                if viewModel.scannedImage != nil {
-                                    applyFilter()
-                                }
-                            }
-                    }
-                }
-
-                Button("View Documents") {
-                    isShowingDocumentsModal.toggle()
-                } .padding()
-                    .foregroundColor(.blue)
-                    .bold()
-                    .padding(.horizontal, 20)
-                    .padding()
-                .sheet(isPresented: $isShowingDocumentsModal) {
-                    DocumentsModalView(savedDocuments: $viewModel.savedDocuments)
-                }.padding()
-            }
-            .navigationBarTitle("Scan Document")
-            .alert(isPresented: $showAlert) {
-                Alert(title: Text("Success"), message: Text("Document saved as PDF"), dismissButton: .default(Text("OK")))
-            }
-        }
-    }
-
-    private func applyFilter() {
-        guard let uiImage = viewModel.scannedImage else { return }
-
-        // Convert UIImage to CIImage
-        let ciImage = CIImage(image: uiImage)
-
-        // Apply a grayscale filter
-        let filter = CIFilter.colorControls()
-        filter.inputImage = ciImage
-        filter.saturation = 0.0
-
-        if let outputCIImage = filter.outputImage {
-            // Convert CIImage back to UIImage
-            let context = CIContext()
-            if let cgImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) {
-                viewModel.scannedImage = UIImage(cgImage: cgImage)
-            }
-        }
-    }
-
-    private func saveImageAsPDF() {
-        guard let scannedImage = viewModel.scannedImage else { return }
-
-        let pdfDocument = PDFDocument()
-        let pdfPage = PDFPage(image: scannedImage)
-        pdfDocument.insert(pdfPage!, at: 0)
-
-        if let data = pdfDocument.dataRepresentation() {
-            do {
-                let documentsURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-                let fileName = "scannedDocument_\(Date().timeIntervalSince1970).pdf"
-                let fileURL = documentsURL.appendingPathComponent(fileName)
-                try data.write(to: fileURL)
-                showAlert = true // Attiva la notifica
-                viewModel.savedDocuments.append(fileURL)
-            } catch {
-                print("Error saving document as PDF: \(error.localizedDescription)")
-            }
-        }
-    }
-}
-
-struct DocumentsModalView: View {
-    @Binding var savedDocuments: [URL]
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(savedDocuments, id: \.self) { documentURL in
-                    NavigationLink(destination: PDFViewer(url: documentURL)) {
-                        Text(documentURL.lastPathComponent)
-                    }
-                }
-            }
-            .navigationBarTitle("Saved Documents")
-        }
-    }
-}
-
-struct PDFViewer: View {
-    let url: URL
-
-    var body: some View {
-        PDFKitView(url: url)
-            .navigationBarTitle(Text(url.lastPathComponent), displayMode: .inline)
-    }
-}
-
-struct PDFKitView: UIViewRepresentable {
-    let url: URL
-
-    func makeUIView(context: Context) -> PDFView {
-        let pdfView = PDFView()
-        pdfView.document = PDFDocument(url: url)
-        pdfView.autoScales = true
-        return pdfView
-    }
-
-    func updateUIView(_ uiView: PDFView, context: Context) {}
-}*/
-
-
-class SharedViewModel: ObservableObject {
-    @Published var scannedImage: UIImage?
-    @Published var savedDocuments: [URL] = []
-}
-
-struct ContentView: View {
-    @StateObject private var viewModel = SharedViewModel()
-    @State private var isImagePickerPresented: Bool = false
-    @State private var showAlert: Bool = false
-    @State private var isShowingDocumentsModal: Bool = false
-
-    var body: some View {
-        NavigationView {
-            VStack {
-                if let scannedImage = viewModel.scannedImage {
-                    Image(uiImage: scannedImage)
-                        .resizable()
-                        .scaledToFit()
+                        .buttonStyle(ViewDocumentsButtonStyle())
+                        .sheet(isPresented: $isShowingDocumentsModal) {
+                            DocumentsModalView(savedDocuments: $viewModel.savedDocuments)
+                        }
                         .padding()
+                        Spacer().frame(height: 10)
 
-                    Button("Save") {
-                        applyFilter()
-                        saveImageAsPDF()
-                        // Reset scannedImage and return to "Capture Photo" screen
-                        withAnimation {
-                            viewModel.scannedImage = nil
-                        }
+                        
                     }
-                    .buttonStyle(DoneButtonStyle())
-                    .padding()
-                } else {
-                    Button(action: {
-                        isImagePickerPresented.toggle()
-                    }) {
-                        CapturePhotoButton()
-                    }
-                    .sheet(isPresented: $isImagePickerPresented) {
-                        ImagePicker(selectedImage: $viewModel.scannedImage, sourceType: .camera)
-                            .onDisappear {
-                                if viewModel.scannedImage != nil {
-                                    applyFilter()
-                                }
-                            }
-                    }
-
-                    Spacer().frame(height: 20)
-                    
-
-                    Button("View Documents") {
-                        isShowingDocumentsModal.toggle()
-                    }
-                    .buttonStyle(ViewDocumentsButtonStyle())
-                    .sheet(isPresented: $isShowingDocumentsModal) {
-                        DocumentsModalView(savedDocuments: $viewModel.savedDocuments)
-                    }
-                    .padding()
-                    Spacer().frame(height: 10)
-                    
-                    
                 }
-            }
-            .navigationBarTitle("Scan Document")
-            .alert(isPresented: $showAlert) {
-                Alert(title: Text("Success"), message: Text("Document saved as PDF"), dismissButton: .default(Text("OK")))
+                .navigationBarTitle("Scan Document")
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text("Success"), message: Text("Document saved as PDF"), dismissButton: .default(Text("OK")))
+                }
             }
         }
         .background(Color("BackgroundColor").ignoresSafeArea())
     }
-
+    
     private func applyFilter() {
         guard let uiImage = viewModel.scannedImage else { return }
-
+        
         // Convert UIImage to CIImage
         let ciImage = CIImage(image: uiImage)
-
+        
         // Apply a grayscale filter
         let filter = CIFilter.colorControls()
         filter.inputImage = ciImage
         filter.saturation = 0.0
-
+        
         if let outputCIImage = filter.outputImage {
             // Convert CIImage back to UIImage
             let context = CIContext()
@@ -254,14 +122,14 @@ struct ContentView: View {
             }
         }
     }
-
+    
     private func saveImageAsPDF() {
         guard let scannedImage = viewModel.scannedImage else { return }
-
+        
         let pdfDocument = PDFDocument()
         let pdfPage = PDFPage(image: scannedImage)
         pdfDocument.insert(pdfPage!, at: 0)
-
+        
         if let data = pdfDocument.dataRepresentation() {
             do {
                 let documentsURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
@@ -279,7 +147,7 @@ struct ContentView: View {
 
 struct DocumentsModalView: View {
     @Binding var savedDocuments: [URL]
-
+    
     var body: some View {
         NavigationView {
             List {
@@ -296,7 +164,7 @@ struct DocumentsModalView: View {
 
 struct PDFViewer: View {
     let url: URL
-
+    
     var body: some View {
         PDFKitView(url: url)
             .navigationBarTitle(Text(url.lastPathComponent), displayMode: .inline)
@@ -305,14 +173,14 @@ struct PDFViewer: View {
 
 struct PDFKitView: UIViewRepresentable {
     let url: URL
-
+    
     func makeUIView(context: Context) -> PDFView {
         let pdfView = PDFView()
         pdfView.document = PDFDocument(url: url)
         pdfView.autoScales = true
         return pdfView
     }
-
+    
     func updateUIView(_ uiView: PDFView, context: Context) {}
 }
 
@@ -330,22 +198,23 @@ struct DoneButtonStyle: ButtonStyle {
 
 struct CapturePhotoButton: View {
     var body: some View {
-        HStack {
+        HStack(spacing: 0) { // Nessuno spazio tra l'icona e il testo
             Image(systemName: "camera.fill")
                 .font(.title)
+                .foregroundColor(.white).offset(x: 10)
+
             Text("Capture photo")
                 .foregroundColor(.white)
+                .bold()
                 .padding()
-            
         }
-        .background(RoundedRectangle(cornerRadius: 15).fill(Color.blue))
+        .background(RoundedRectangle(cornerRadius: 15).fill(Color.black))
         .padding(.horizontal, 20)
         .shadow(radius: 5)
         .padding()
-        
     }
-    
 }
+
 
 struct ViewDocumentsButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
@@ -358,8 +227,6 @@ struct ViewDocumentsButtonStyle: ButtonStyle {
             .padding()
     }
 }
-
-
 #Preview {
     ContentView()
 }
